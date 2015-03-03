6c264e22f559a8f4ef54f20982777466b782bdcf
Jonathan Naylor <naylorjs@yahoo.com>
2015-03-03 20:57:07 +0000

    The 20150303 release of the ircDDB Gateway.

--- ircDDBGateway/Common/Version.h
+++ ircDDBGateway/Common/Version.h
@@ -24,9 +24,9 @@
 const wxString VENDOR_NAME = wxT("G4KLX");
 
 #if defined(__WXDEBUG__)
-const wxString VERSION = wxT("20150213 - DEBUG");
+const wxString VERSION = wxT("20150303 - DEBUG");
 #else
-const wxString VERSION = wxT("20150213");
+const wxString VERSION = wxT("20150303");
 #endif
 
 #endif
