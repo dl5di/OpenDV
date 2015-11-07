449473dbddcdba7d76714ab49428a71f72cd6fe7
John Hays <john@hays.org>
2015-11-06 20:40:01 -0800

    Fixed so would compile on PI 2 -- missing GLOBAL Definitions

--- DStarRepeater/settings_raspbian_armhf_pi2.mk
+++ DStarRepeater/settings_raspbian_armhf_pi2.mk
@@ -6,7 +6,9 @@
 #
 	export DATADIR := "/home/opendv/data"
 	export BINDIR  := "$(DESTDIR)/usr/bin"
+	export LOGDIR  := "/var/log/opendv"
+	export CONFDIR := "/home/opendv/dstarrepeater"
 	export CC      := $(shell wx-config --cxx)
 	export LDFLAGS := 
-	export CFLAGS  := -O2 -march=armv7-a -mfpu=vfp -mfloat-abi=hard -Wall -Wno-non-virtual-dtor -Wno-strict-aliasing -Wno-psabi -DDATA_DIR='$(DATADIR)' -DBIN_DIR='$(BINDIR)' -D_FILE_OFFSET_BITS=64 -D_LARGE_FILES -D__WXGTK__ -DGPIO -pthread $(shell wx-config --cxxflags)
+	export CFLAGS  := -O2 -march=armv7-a -mfpu=vfp -mfloat-abi=hard -Wall -Wno-non-virtual-dtor -Wno-strict-aliasing -Wno-psabi -DLOG_DIR='$(LOGDIR)' -DCONF_DIR='$(CONFDIR)' -DDATA_DIR='$(DATADIR)' -DBIN_DIR='$(BINDIR)' -D_FILE_OFFSET_BITS=64 -D_LARGE_FILES -D__WXGTK__ -DGPIO -pthread $(shell wx-config --cxxflags)
 	export LIBS    := -lasound -lusb-1.0 -lwiringPi $(shell wx-config --libs adv,core)
