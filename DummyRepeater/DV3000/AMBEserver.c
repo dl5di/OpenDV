5725fcfd92ccd510e1818fdaa79d03005a581639
Jeremy McDermond <nh6z@nh6z.net>
2016-04-07 14:48:12 -0700

    AMBEserver:  Rewrites and cleanup
    Rewrote a lot of the server process to be cleaner and easier to understand.
    
    	*  Fixed up error messages so they return the result of strerror()
               instead of just spitting out errno.
    	*  Removed the '-c' flag as it made no sense with the majority of the
               serial ports that a AMBE device could appear on.  '-i' provides
               any of the functionality provided by '-c'.
    	*  Error checking on the '-p' flag.
    	*  Replaced the '-n' flag with the '-s' flag for speed.  You now set
               the speed by giving the program a baud rate number.  We should
               be able to set at least 230400 and 406800.  Any additional rates
               should be easily supportable.
    	*  Added an '-r' flag to enable hardware reset of a PiDV on the
               Raspberry Pi.  This flag is only available on the versions
               compiled with --with-gpio.  This eliminates the need for an
               AMBEserverGPIO variant of the executable.
    	*  A bunch of general cleanup doing bounds checking on strings,
               eliminating global variables and streamlining IO.

--- DummyRepeater/DV3000/AMBEserver.c
+++ DummyRepeater/DV3000/AMBEserver.c
@@ -1,5 +1,6 @@
 /*
  *   Copyright (C) 2014 by Jonathan Naylor G4KLX and John Hays K7VE
+ *   Copyright 2016 by Jeremy McDermond (NH6Z)
  *
  *   This program is free software; you can redistribute it and/or modify
  *   it under the terms of the GNU General Public License as published by
@@ -32,21 +33,28 @@
 #include <sys/types.h>
 #include <sys/select.h>
 #include <sys/socket.h>
+#include <sys/param.h>
 
 #include <netinet/in.h>
+
 #if defined(RASPBERRY_PI)
 #include <wiringPi.h>
-#else
-int  wiringPiSetup();
-void pinMode(int pin, int mode);
-void digitalWrite(int pin, int value);
-void delay(unsigned int delay);
-#define OUTPUT  1
-#define HIGH    1
-#define LOW     0
 #endif
 
-void delay(unsigned int delay) {
+#define dv3k_packet_size(a) (1 + sizeof((a).header) + ntohs((a).header.payload_length))
+
+#pragma pack(push, 1)
+struct dv3k_packet {
+    unsigned char start_byte;
+    struct {
+        unsigned short payload_length;
+        unsigned char packet_type;
+    } header;
+    char payload[325];
+};
+#pragma pack(pop)
+
+static inline void delay(unsigned int delay) {
 	struct timespec tim, tim2;
 	tim.tv_sec = 0;
 	tim.tv_nsec = delay * 1000UL;
@@ -55,30 +63,24 @@ void delay(unsigned int delay) {
 
 #define	DV3000_VERSION		"2015-07-11"
 
-#define	AMBE3000_HEADER_LEN	4U
-#define	AMBE3000_START_BYTE	0x61U
+#define	AMBE3000_START_BYTE	0x61
 
-#define	BUFFER_LENGTH		400U
-#define	DEFAULT_PORT		2460U
+#define	DEFAULT_PORT		2460
 #define DEFAULT_TTY		"/dev/ttyUSB0"
-#define DEFAULT_BAUD		230400U
+#define DEFAULT_BAUD		230400
 
+struct sockaddr_in sa = { 0 };
+static int debug = 0;
 
-
-struct sockaddr_in sa;
-int serialFd;
-int sockFd;
-int debug = 0;
-char dv3000tty[50] = "";
-
-void dump(char *text, unsigned char *data, unsigned int length)
+void dump(char *text, struct dv3k_packet *packet, unsigned int length)
 {
 	unsigned int offset = 0U;
 	unsigned int i;
+	unsigned char *data = (unsigned char *) packet;
 
-	fputs(text, stdout);
-	fputc('\n', stdout);
-
+	fprintf(stdout, "Received %s packet with type %d and length %d:\n", text, 
+		packet->header.packet_type, ntohs(packet->header.payload_length));
+		
 	while (length > 0U) {
 		unsigned int bytes = (length > 16U) ? 16U : length;
 
@@ -114,7 +116,6 @@ void dump(char *text, unsigned char *data, unsigned int length)
 }
 
 #if defined(RASPBERRY_PI)
-
 int openWiringPi(void)
 {
         int ret = wiringPiSetup();
@@ -127,11 +128,8 @@ int openWiringPi(void)
 
         // Reset the hardware
         digitalWrite(7, LOW);
-
         delay(20UL);
-
         digitalWrite(7, HIGH);
-
         delay(750UL);
 
         if (debug)
@@ -139,101 +137,105 @@ int openWiringPi(void)
 
         return 1;
 }
-
 #endif
 
+const char reset[] = { 0x61, 0x00, 0x01, 0x00, 0x33 };
+const char prodId[] = { 0x61, 0x00, 0x01, 0x00, 0x33 };
 
-int openSerial(long baud)
+int openSerial(char *ttyname, long baud)
 {
 	struct termios tty;
 	int fd;
-        int n1;
-        char reset[5];
-        reset[0] = 0x61;
-        reset[1] = 0x00;
-        reset[2] = 0x01;
-        reset[3] = 0x00;
-        reset[4] = 0x33;
-
-
-	char prodID[5];
-        prodID[0] = 0x61;
-        prodID[1] = 0x00;
-        prodID[2] = 0x01;
-        prodID[3] = 0x00;
-        prodID[4] = 0x33;
+    ssize_t bytesWritten;
 	
-	fd = open(dv3000tty, O_RDWR | O_NOCTTY | O_SYNC);
+	fd = open(ttyname, O_RDWR | O_NOCTTY | O_SYNC);
 	if (fd < 0) {
-		fprintf(stderr, "AMBEserver: error when opening %s, errno=%d\n", dv3000tty,errno);
+		fprintf(stderr, "AMBEserver: error when opening %s: %s\n", ttyname, strerror(errno));
 		return fd;
 	}
 
 	if (tcgetattr(fd, &tty) != 0) {
-		fprintf(stderr, "AMBEserver: error %d from tcgetattr\n", errno);
+		fprintf(stderr, "AMBEserver tcgetattr: %s\n", strerror(errno));
 		return -1;
 	}
-	if(debug) fprintf(stderr, "Baud %ld\n", baud);
-	if(baud == 230400) {
-		cfsetospeed(&tty, B230400);
-		cfsetispeed(&tty, B230400);
-	} else {
-		cfsetospeed(&tty, B460800);
-		cfsetispeed(&tty, B460800);
+	
+	if(debug)
+		fprintf(stderr, "Baud %ld\n", baud);
+		
+	//  Input speed = output speed
+	cfsetispeed(&tty, B0);
+	
+	switch(baud) {
+		case 230400:
+			cfsetospeed(&tty, B230400);
+			break;
+		case 460800:
+			cfsetospeed(&tty, B460800);
+			break;
+		default:
+			fprintf(stderr, "AMBEserver: unsupported baud rate %ld\n", baud);
+			return -1;
 	}
-	tty.c_cflag = (tty.c_cflag & ~CSIZE) | CS8;
-	tty.c_iflag &= ~IGNBRK;
-	tty.c_lflag = 0;
-
-	tty.c_oflag = 0;
-	tty.c_cc[VMIN]  = 0;
-	tty.c_cc[VTIME] = 5;
-
-	tty.c_iflag &= ~(IXON | IXOFF | IXANY);
-
-	tty.c_cflag |= (CLOCAL | CREAD);
-
-	tty.c_cflag &= ~(PARENB | PARODD);
-	tty.c_cflag &= ~CSTOPB;
-	tty.c_cflag &= ~CRTSCTS;
-
+	
+	tty.c_lflag    &= ~(ECHO | ECHOE | ICANON | IEXTEN | ISIG);
+    tty.c_iflag    &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON | IXOFF | IXANY);
+    tty.c_cflag    &= ~(CSIZE | CSTOPB | PARENB);
+    tty.c_cflag    |= CS8 | CRTSCTS;
+    tty.c_oflag    &= ~(OPOST);
+    tty.c_cc[VMIN] = 0;
+    tty.c_cc[VTIME] = 1;
+	
 	if (tcsetattr(fd, TCSANOW, &tty) != 0) {
-		fprintf(stderr, "AMBEserver: error %d from tcsetattr\n", errno);
+		fprintf(stderr, "AMBEserver: tcsetattr: %s\n", strerror(errno));
 		return -1;
 	}
 
 	if (debug)
-		fprintf(stdout, "opened %s %ld\n",dv3000tty,baud);
-	n1 = write(fd,reset,5);
+		fprintf(stdout, "opened %s %ld\n", ttyname, baud);
+		
+	bytesWritten = write(fd, reset, sizeof(reset));
+	if(bytesWritten != sizeof(reset)) {
+		if(bytesWritten == -1)
+			fprintf(stderr, "Error writing reset sequence: %s\n", strerror(errno));
+		else
+			fprintf(stderr, "Short write on reset sequence\n");
+		return -1;
+	}
 	if (debug)
-		fprintf(stderr,"Wrote Reset %d chars\n",n1);
-	n1 = write(fd,prodID,5);
+		fprintf(stderr, "Wrote Reset %ld chars\n", bytesWritten);
+		
+	bytesWritten = write(fd ,prodId, sizeof(prodId));
+	if(bytesWritten != sizeof(prodId)) {
+		if(bytesWritten == -1)
+			fprintf(stderr, "Error writing Product ID command: %s\n", strerror(errno));
+		else
+			fprintf(stderr, "Short write on Product ID command");
+		return -1;
+	}
 	if (debug)
-		fprintf(stderr,"Wrote prodID  %d chars\n",n1);
+		fprintf(stderr,"Wrote prodID %ld chars\n", bytesWritten);
+		
 	return fd; 
 }
 
-int openSocket(uint16_t port)
+int openSocket(in_port_t port)
 {
-	struct sockaddr_in sa;
-	int ret;
+	struct sockaddr_in sa = { 0 };
 	int fd;
 
 	fd = socket(AF_INET, SOCK_DGRAM, 0);
 	if (fd < 0) {
-		fprintf(stderr, "AMBEserver: error when creating the socket, errno=%d", errno);
+		fprintf(stderr, "AMBEserver: error when creating the socket: %s\n", strerror(errno));
 		return fd;
 	}
 
-	memset(&sa, 0x00, sizeof(struct sockaddr_in));
 	sa.sin_family = AF_INET;
 	sa.sin_port = htons(port);
 	sa.sin_addr.s_addr = htonl(INADDR_ANY);
 
-	ret = bind(fd, (struct sockaddr *)&sa, sizeof(struct sockaddr_in));
-	if (ret < 0) {
-		fprintf(stderr, "AMBEserver: error when binding the socket to port %u, errno=%d", port, errno);
-		return fd;
+	if (bind(fd, (struct sockaddr *) &sa, sizeof(sa)) < 0) {
+		fprintf(stderr, "AMBEserver: error when binding the socket to port %u: %s\n", port, strerror(errno));
+		return -1;
 	}
 
 	if (debug)
@@ -242,128 +244,144 @@ int openSocket(uint16_t port)
 	return fd;
 }
 
-int processSerial(void)
+int processSerial(int sockFd, int serialFd)
 {
-	unsigned char buffer[BUFFER_LENGTH];
-	unsigned int respLen;
-	unsigned int offset;
-	ssize_t len;
-
-	len = read(serialFd, buffer, 1);
-	if (len != 1) {
-		fprintf(stderr, "AMBEserver: error when reading from the serial port, errno=%d", errno);
+	ssize_t bytes, bytesLeft;
+	struct dv3k_packet packet;
+	ssize_t bytesRead;
+	
+	for(int i = 0; i < sizeof(struct dv3k_packet); ++i) {
+		bytesRead = read(serialFd, &packet, 1);
+		if(bytes == -1) {
+			fprintf(stderr, "Error reading from serial port: %s\n", strerror(errno));
+			return 0;
+		}
+		if(packet.start_byte == AMBE3000_START_BYTE)
+			break;
+	}
+	if(packet.start_byte != AMBE3000_START_BYTE) {
+		fprintf(stderr, "Couldn't find start byte in serial data\n");
 		return 0;
 	}
 
-	if (buffer[0U] != AMBE3000_START_BYTE) {
-		fprintf(stderr, "AMBEserver: unknown byte from the DV3000, 0x%02X", buffer[0U]);
-		return 1;
-	}
-
-	offset = 0U;
-	while (offset < (AMBE3000_HEADER_LEN - 1U)) {
-		len = read(serialFd, buffer + 1U + offset, AMBE3000_HEADER_LEN - 1 - offset);
-
-		if (len == 0)
-			delay(5UL);
-
-		offset += len;
-	}
-
-	respLen = buffer[1U] * 256U + buffer[2U];
-
-	offset = 0U;
-	while (offset < respLen) {
-		len = read(serialFd, buffer + AMBE3000_HEADER_LEN + offset, respLen - offset);
-
-		if (len == 0)
-			delay(5UL);
-
-		offset += len;
-	}
-
-	respLen += AMBE3000_HEADER_LEN;
-
-	if (debug)
-		dump("Serial data", buffer, respLen);
-
-	if (sa.sin_port != 0U) {
-		len = sendto(sockFd, buffer, respLen, 0, (struct sockaddr *)&sa, sizeof(struct sockaddr_in));
-		if (respLen != len) {
-			fprintf(stderr, "AMBEserver: error when writing to the socket, len=%zd, errno=%d", len, errno);
+	bytesLeft = sizeof(packet.header);
+	while(bytesLeft > 0) {
+		bytes = read(serialFd, ((uint8_t *) &packet.header) + sizeof(packet.header) - bytesLeft, bytesLeft);
+		if(bytes == -1) {
+			fprintf(stderr, "Couldn't read serial data header\n");
 			return 0;
 		}
+		bytesLeft -= (size_t) bytes;
+	}
+	
+	bytesLeft = ntohs(packet.header.payload_length);
+    if(bytesLeft > sizeof(packet.payload)) {
+        fprintf(stderr, "Serial payload exceeds buffer size: %ld\n", bytesLeft);
+        return 0;
+    }
+    
+    while(bytesLeft > 0) {
+        bytes = read(serialFd, ((uint8_t *) &packet.payload) + (ntohs(packet.header.payload_length) - bytesLeft), bytesLeft);
+        if(bytes == -1) {
+            fprintf(stderr, "Couldn't read payload: %s\n", strerror(errno));
+            return 0;
+        }
+        
+        bytesLeft -= (size_t) bytes;
+    }
+
+	if (debug)
+		dump("Serial data", &packet, dv3k_packet_size(packet));
+
+	if (sa.sin_port != 0U)
+		if (sendto(sockFd, &packet, dv3k_packet_size(packet), 0, (struct sockaddr *)&sa, sizeof(struct sockaddr_in)) != dv3k_packet_size(packet)) {
+			fprintf(stderr, "AMBEserver: error when writing to the socket: %s\n", strerror(errno));
+			return 0;
 	}
 
 	return 1;
 }
 
-int processSocket(void)
+int processSocket(int sockFd, int serialFd)
 {
-	unsigned char buffer[BUFFER_LENGTH];
-	socklen_t sl = sizeof(struct sockaddr_in);
-	unsigned int len;
-	ssize_t n;
+	socklen_t addrLen = sizeof(struct sockaddr_in);
+	ssize_t bytesRead;
+	struct dv3k_packet packet;
 	
-	n = recvfrom(sockFd, buffer, BUFFER_LENGTH, 0, (struct sockaddr *)&sa, &sl);
-	if (n < 0) {
-		fprintf(stderr, "AMBEserver: error when reading from the socket, errno=%d", errno);
+	bytesRead = recvfrom(sockFd, &packet, sizeof(packet), 0, (struct sockaddr *) &sa, &addrLen);
+	if (bytesRead < 0) {
+		fprintf(stderr, "AMBEserver: error when reading from the socket: %s\n", strerror(errno));
 		return 0;
 	}
 
 	if (debug)
-		dump("Socket data", buffer, n);
+		dump("Socket data", &packet, bytesRead);
 
-	if (buffer[0U] != AMBE3000_START_BYTE) {
-		fprintf(stderr, "AMBEserver: invalid start byte when reading from the socket, 0x%02X", buffer[0U]);
+	if (packet.start_byte != AMBE3000_START_BYTE) {
+		fprintf(stderr, "AMBEserver: invalid start byte when reading from the socket, 0x%02X", packet.start_byte);
 		return 1;
 	}
 
-	len = buffer[1U] * 256U + buffer[2U] + AMBE3000_HEADER_LEN;
-	if (n != len) {
-		fprintf(stderr, "AMBEserver: invalid length when reading from the socket, %u=%zd", len, n);
+	if (bytesRead != dv3k_packet_size(packet)) {
+		fprintf(stderr, "AMBEserver: invalid length when reading from the socket, %ld=%zd", bytesRead, dv3k_packet_size(packet));
 		return 1;
 	}
 
-	n = write(serialFd, buffer, len);
-	if (n != len) {
-		fprintf(stderr, "AMBEserver: error when writing to the serial port, errno=%d", errno);
+	if (write(serialFd, &packet, dv3k_packet_size(packet)) != dv3k_packet_size(packet)) {
+		fprintf(stderr, "AMBEserver: error when writing to the serial port: %s\n", strerror(errno));
 		return 0;
 	}
 
 	return 1;
+}
+
+void usage() {
+	fprintf(stderr, "Usage: AMBEserver [-d] [-n] [-p port] [-i tty] [-v] [-x]\n");
+	fprintf(stderr, "Selecting -n uses 460800 baud (new) datarate\n");
+	exit(1);
 }
 
 int main(int argc, char **argv)
 {
-	uint16_t port = DEFAULT_PORT;
+	in_port_t port = DEFAULT_PORT;
 	long baud = DEFAULT_BAUD;
-
-	int daemon = 0;
-	int topFd;
-	int commnum;
+	char dv3000tty[MAXPATHLEN] = DEFAULT_TTY;
+	
 	fd_set fds;
-	int ret;
+	int serialFd;
+	int sockFd;
+	int topFd;
+	
 	int c;
-	strcpy(dv3000tty,DEFAULT_TTY);
+	
+#if defined(RASPBERRY_PI)
+	char reset = 0;
+#endif
+	char daemon = 0;
 
-	while ((c = getopt(argc, argv, "ndc:p:i:vxh")) != -1) {
+	while ((c = getopt(argc, argv, "s:dp:i:vxh")) != -1) {
 		switch (c) {
 			case 'd':
 				daemon = 1;
 				break;
 			case 'p':
-				port = (uint16_t)atoi(optarg);
+				errno = 0;
+				port = strtol(optarg, NULL, 10);
+				if(errno != 0 || port == 0) {
+					fprintf(stderr, "Invalid port number: %s\n", optarg);
+					usage();
+				}
 				break;
-			case 'n':
-				baud = 460800;
-				break;
-			case 'c':
-				commnum = (uint16_t)atoi(optarg);
-				sprintf(dv3000tty,"/dev/ttyS%d",commnum - 1);
+			case 's':
+				errno = 0;
+				baud = strtol(optarg, NULL, 10);
+				if(errno != 0 || baud == 0) {
+					fprintf(stderr, "Invalid baud rate: %s\n", optarg);
+					usage();
+				}
 				break;
 			case 'i':
-				strcpy(dv3000tty,optarg);
+				strncpy(dv3000tty, optarg, sizeof(dv3000tty));
 				break;
 			case 'v':
 				fprintf(stdout, "AMBEserver: version " DV3000_VERSION "\n");
@@ -371,14 +389,20 @@ int main(int argc, char **argv)
 			case 'x':
 				debug = 1;
 				break;
+#if defined(RASPBERRY_PI)
+			case 'r':
+				reset = 1;
+				break;
+#endif
 			case 'h':
 			default:
-				fprintf(stderr, "Usage: AMBEserver [-d] [-n] [-p port] [-i tty] [-c commportnum] [-v] [-x]\nSelecting -n uses 460800 baud (new) datarate\n");
-				return 1;
+				usage();
+				break;
 		}
 	}
+	
 	if (strlen(dv3000tty) < 1) {
-		fprintf(stderr, "An input tty filename (-i /dev/ttyXXX) or a COM port number (-c 3) must be set.\n");
+		fprintf(stderr, "An input tty filename (-i /dev/ttyXXX) must be set.\n");
 		return 1;
 	}
 
@@ -387,69 +411,58 @@ int main(int argc, char **argv)
 
 		if (pid < 0) {
 			fprintf(stderr, "AMBEserver: error in fork(), exiting\n");
-			return 1;
+			exit(1);
 		}
 
 		// If this is the parent, exit
 		if (pid > 0)
-			return 0;
+			exit(0);
 
 		// We are the child from here onwards
 		setsid();
-
-//		chdir("/");
 
 		umask(0);
 	}
 
 #if defined(RASPBERRY_PI)
-        ret = openWiringPi();
-        if (!ret) {
-		fprintf(stderr,"Unable to open WiringPi, exiting\n");
-                return 1;
-	} else {
-		fprintf(stderr,"Reset DV3000\n");
+	if(reset) {
+		if (!openWiringPi()) {
+			fprintf(stderr,"Unable to open WiringPi, exiting\n");
+			exit(1);
+		} else {
+			fprintf(stderr,"Reset DV3000\n");
+		}
 	}
-
 #endif
 
 	sockFd = openSocket(port);
 	if (sockFd < 0)
-		return 1;
+		exit(1);
 		
-	serialFd = openSerial(baud);
+	serialFd = openSerial(dv3000tty, baud);
 	if (serialFd < 0)
-		return 1;
+		exit(1);
 
-	memset(&sa, 0x00, sizeof(struct sockaddr_in));
-
-	topFd = sockFd;
-	if (serialFd > topFd)
-		topFd = serialFd;
-	topFd++;
+	topFd = (sockFd > serialFd ? sockFd : serialFd ) + 1;
 
 	for (;;) {
 		FD_ZERO(&fds);
 		FD_SET(sockFd, &fds);
 		FD_SET(serialFd, &fds);
-		ret = select(topFd, &fds, NULL, NULL, NULL);
-		if (ret < 0) {
-			fprintf(stderr, "AMBEserver: error from select, errno=%d\n", errno);
-			return 1;
+
+		if (select(topFd, &fds, NULL, NULL, NULL) < 0) {
+			fprintf(stderr, "AMBEserver: error from select: %s\n", strerror(errno));
+			exit(1);
 		}
 		
-		if (FD_ISSET(sockFd, &fds)) {
-			ret = processSocket();
-			if (!ret)
-				return 1;
-		}
+		if(FD_ISSET(sockFd, &fds))
+			if (!processSocket(sockFd, serialFd))
+				exit(1);
 
-		if (FD_ISSET(serialFd, &fds)) {
-			ret = processSerial();
-			if (!ret)
-				return 1;
-		}
+		if(FD_ISSET(serialFd, &fds))
+			if (!processSerial(sockFd, serialFd))
+				exit(1);
 	}
 
-	return 0;
+	exit(0);
 }
